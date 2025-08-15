33d0aafd595818a42d0d8693c4cc7839330a7a40
shubhamNayak1 <itssnayak@gmail.com>
2025-08-15 05:55:20 +0530

    new nav bar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,12 +4,25 @@ import { FaTachometerAlt, FaUser } from 'react-icons/fa';
 import { TbPasswordUser } from "react-icons/tb";
 import { LogOut } from 'lucide-react';
 import './Navbar.css';
+import DialogBox from '../dialogBox/dialogBox';
 
 const Navbar: React.FC = () => {
   const [openMenu, setOpenMenu] = useState<string | null>(null);
   const [accountMenuOpen, setAccountMenuOpen] = useState<boolean>(false);
   const [username, setUsername] = useState<string>('');
   const navigate = useNavigate();
+  const [resetPassword, setResetPassword] = useState(false);
+
+  const handleResetPasswordOpen = () => {
+    setResetPassword(true);
+  };
+
+  const handleResetPasswordClose = () => {
+    setResetPassword(false);
+  };
+
+  const handleResetPasswordSave = () => {
+  };
 
   useEffect(() => {
     const storedUsername = localStorage.getItem('username');
@@ -34,6 +47,14 @@ const Navbar: React.FC = () => {
     setAccountMenuOpen(!accountMenuOpen);
   };
 
+  const [submenuPos, setSubmenuPos] = useState({ top: 0 });
+
+  const handleAdminClick = (e: { currentTarget: { getBoundingClientRect: () => any; }; }) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    setSubmenuPos({ top: rect.top + 10});
+    setOpenMenu(openMenu === 'admin' ? null : 'admin');
+  };
+  
   return (
     <div className="navbar">
       <div className="logo-setter">
@@ -53,7 +74,9 @@ const Navbar: React.FC = () => {
         </div>
 
         {/* Admin */}
-        <div className={`clickable-div ${openMenu === 'admin' ? 'clicked' : ''}`}>
+        <div className={`clickable-div ${openMenu === 'admin' ? 'clicked' : ''}`}
+          onClick={handleAdminClick}
+        >
           <div
             className="subnavMenu"
             onClick={() => setOpenMenu(openMenu === 'admin' ? null : 'admin')}
@@ -64,7 +87,9 @@ const Navbar: React.FC = () => {
 
           {/* Show submenu only while hovering or until click */}
           {openMenu === 'admin' && (
-            <div className="admin-submenu">
+            <div className="admin-submenu"
+              style={{ top: submenuPos.top}}
+            >
               <div onClick={() => handleMenuClick('admin-user', '/admin/user')}>Users</div>
               <div onClick={() => handleMenuClick('admin-department', '/admin/department')}>Departments</div>
               <div onClick={() => handleMenuClick('admin-password', '/admin/password-policy')}>Password&nbsp;Policy</div>
@@ -80,7 +105,7 @@ const Navbar: React.FC = () => {
           <div className="account-menu">
             <div className="account-name">{username}</div>
             <hr />
-            <div className="account-change" onClick={handleLogout}>
+            <div className="account-change" onClick={() => handleResetPasswordOpen()}>
               Change Password&nbsp;<TbPasswordUser size={40}/>
             </div>
             <div className="account-item" onClick={handleLogout}>
@@ -89,6 +114,46 @@ const Navbar: React.FC = () => {
           </div>
         )}
       </div>
+      <DialogBox
+        isOpen={resetPassword}
+        onClose={handleResetPasswordClose}
+        onSave={handleResetPasswordSave}
+        title={'Reset Password'}
+      >
+        <div className="input-password-group">
+            <label htmlFor="password">Old Password</label>
+            <input
+              type="text"
+              name="oldPassword"
+              id="oldPassword"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+        </div>
+        <div className="input-password-group">
+            <label htmlFor="password">New Password</label>
+            <input
+              type="text"
+              name="newPassword"
+              id="newPassword"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+        </div>
+        <div className="input-password-group">
+            <label htmlFor="password">Re-Enter New Password</label>
+            <input
+              type="text"
+              name="reEnterNewPassword"
+              id="reEnterNewPassword"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+        </div>
+      </DialogBox>
     </div>
   );
 };
